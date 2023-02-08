fb0c001eb5e8cfe7a6daf8d5946f8b2c5f050c9e
DESKTOP-509JDPP\38343 <dh53689@ubt-uni.net>
2023-02-08 22:57:58 +0100

    Update Supplier.cs

--- MVC_Project/MVC_Project/Models/Supplier/Supplier.cs
+++ MVC_Project/MVC_Project/Models/Supplier/Supplier.cs
@@ -1,6 +1,22 @@
-﻿namespace MVC_Project.Models.Supplier
+﻿using MVC_Project.Wrappers;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Project.Models.Supplier
 {
-    public class Supplier
+    public class Supplier : BaseEntity
     {
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string ContactNumber { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Address { get; set; }
+        public DateTime createdAt { get; set; } = DateTime.Now;
+
+        public DateTime updatedAt { get; set; }
+
     }
 }
